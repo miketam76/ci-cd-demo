644b028881ee87bfd48cb0692504fb787b66df07
Mike Moon Tam <miketam076@gmail.com>
2025-11-05 13:19:07 -0500

    Made another change to index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from CI/CD demo!');
-    res.send("Added this line");
+    res.send('Hello World from CI/CD demo! - VERSION 2');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
